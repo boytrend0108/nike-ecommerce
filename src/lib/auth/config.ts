0f1ef7c64b7d891c4521df651a482c67b0cff6e0
boytrend0108 <boytrend@gmail.com>
2025-08-24 05:58:57 +0300

    fix: aut config

--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,16 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
-import * as schema from "../db/schema";
+import { account } from "../db/schema/account";
+import { session } from "../db/schema/session";
+import { user } from "../db/schema/user";
+import { verification } from "../db/schema/verification";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
-      user: schema.user,
-      session: schema.session,
-      account: schema.account,
-      verification: schema.verification,
+      user: user,
+      session: session,
+      account: account,
+      verification: verification,
     },
   }),
   emailAndPassword: {
